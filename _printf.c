7f0d1132c04411bb6053b02620e8570436e5791c
siham10927 <10927@holbertonstudents.com>
2025-06-20 12:29:40 +0000

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- _printf.c
+++ _printf.c
@@ -1,78 +1,30 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 /**
-  * find_function - function that finds formats for _printf
-  * calls the corresponding function.
-  * @format: format (char, string, int, decimal)
-  * Return: NULL or function associated ;
-  */
-int (*find_function(const char *format))(va_list)
-{
-	unsigned int i = 0;
-	code_f find_f[] = {
-		{"c", print_char},
-		{"s", print_string},
-		{"i", print_int},
-		{"d", print_dec},
-		{"r", print_rev},
-		{"b", print_bin},
-		{"u", print_unsig},
-		{"o", print_octal},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot13},
-		{NULL, NULL}
-	};
-
-	while (find_f[i].sc)
-	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
-		i++;
-	}
-	return (NULL);
-}
-/**
-  * _printf - function that produces output according to a format.
-  * @format: format (char, string, int, decimal)
-  * Return: size the output text;
-  */
+ * _printf - Entry point
+ * @format: char
+ * Return:size
+ */
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	int (*f)(va_list);
-	unsigned int i = 0, cprint = 0;
+	int i = 0, size = 0;
+	va_list args;
 
-	if (format == NULL)
+	va_start(args, format);
+	if (!format || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
-	va_start(ap, format);
-	while (format[i])
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		while (format[i] != '%' && format[i])
+		if (format[i] == '%')
+		{
+			size += get_fun(format[i + 1], args);
+			i += 1;
+		}
+		else
 		{
 			_putchar(format[i]);
-			cprint++;
-			i++;
+			size += 1;
 		}
-		if (format[i] == '\0')
-			return (cprint);
-		f = find_function(&format[i + 1]);
-		if (f != NULL)
-		{
-			cprint += f(ap);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		cprint++;
-		if (format[i + 1] == '%')
-			i += 2;
-		else
-			i++;
 	}
-	va_end(ap);
-	return (cprint);
+	va_end(args);
+	return (size);
 }
